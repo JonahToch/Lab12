b710d0a395857e243e9afe06990f6050b945f8e3
Jonah Toch <jonahet2@illinois.edu>
2018-04-22 15:05:25 -0500

    added buttons

--- app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
@@ -22,7 +22,7 @@ import org.json.JSONObject;
  */
 public final class MainActivity extends AppCompatActivity {
     /** Default logging tag for messages from the main activity. */
-    private static final String TAG = "Lab12:Main";
+    private static final String TAG = "Lab1:Main";
 
     /** Request queue for our API requests. */
     private static RequestQueue requestQueue;
